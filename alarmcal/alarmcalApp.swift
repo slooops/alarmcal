c2223986894d4f3674918dc1cae70f01252b3804
jack <82819874+slooops@users.noreply.github.com>
2026-06-13 18:45:02 -0700

    Initial Commit

---
+++ alarmcal/alarmcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  alarmcalApp.swift
+//  alarmcal
+//
+//  Created by jasloop on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct alarmcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ alarmcalTests/alarmcalTests.swift
@@ -0,0 +1,19 @@
+//
+//  alarmcalTests.swift
+//  alarmcalTests
+//
+//  Created by jasloop on 6/13/26.
+//
+
+import Testing
+@testable import alarmcal
+
+struct alarmcalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
